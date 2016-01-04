f2adc3434960e41126c9f43ce32f0a32283cb5f4
Matteo Scandolo <teo@onlab.us>
2016-01-04 11:52:21 -0800

    loader

--- components/daily_commits.jsx
+++ components/daily_commits.jsx
@@ -4,12 +4,13 @@ import { connect } from 'react-redux';
 const DailyCommits = ({dailyCommits}) => (
   <div>
     <h1>Todos</h1>
-    {dailyCommits.map(item => <p key={item.id}>{item.dev_name}</p>)}
+    {dailyCommits.isFetching ? 'loading':null}
+    {dailyCommits.data.map(item => <p key={item.id}>{item.dev_name}</p>)}
   </div>
 )
 
 function mapStateToProps(store) {
-  const dailyCommits = store.dailyCommits.data;
+  const dailyCommits = store.dailyCommits;
   console.log('DAILY COMMITS', dailyCommits);
   return {
     dailyCommits
